f74876078d71b677eb404d707081ceec94fb94fe
Jorge Hernandez <diego.hernandez@i3television.es>
2016-12-15 19:14:34 +0100

    component upgraded to container - binded action fetch all

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-	return <div>List of blogs</div>;
+class PostIndex extends Component {
+	componentWillMount(){
+		console.log("now call action creator to fetch posts");
+		this.props.fetchPosts();
+	}
+	render(){
+		return (
+			<div>List of blog posts</div>
+		);
+	}
 }
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostIndex);
+
+//Refactor: delete mapDispathcToProps and replace export default to:
+//export default connect(null, fetchPosts: fetchPosts)(PostIndex);
+//or in es6:
+//export default connect(null, {fetchPosts})(PostIndex);
