c6a9977e5881855ceb5ddf308fa9a41c74d20b0f
Jorge Hernandez <diego.hernandez@i3television.es>
2016-12-16 14:00:36 +0100

    new post form created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory} from 'react-router';
+
 import promise from 'redux-promise';
 import axios from 'axios';
 
