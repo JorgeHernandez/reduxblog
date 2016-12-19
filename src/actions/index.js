4f915c9f12acc93970248dd356cae88a9ba9da3d
Jorge Hernandez <diego.hernandez@i3television.es>
2016-12-19 18:25:50 +0100

    added new post action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=99999999999999999999';//random key - no registration required
@@ -13,3 +15,12 @@ export function fetchPosts(){
 		payload: request
 	}
 } 
+
+export function createPost(props){
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`);
+
+	return {
+		type: CREATE_POST,
+		payload: request
+	}
+}
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,28 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
 	render(){
+		const handleSubmit = this.props.handleSubmit;
+		//ES6 version
+		//const { handleSubmit } = this.props;
+
+		const title = this.props.title;
+		const categories = this.props.categories;
+		const content = this.props.content;
+
 		return(
-			<form>
+			<form onSubmit={handleSubmit} >
 				<h3>Create a New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...title} />
 				</div>
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>
 				<div className="form-group">
 					<label>Content</label>
-					<textarea className="form-control" rows="10" />
+					<textarea className="form-control" rows="10" {...content} />
 				</div>
 				<button type="submit" className="btn btn-primary">
 					Submit
