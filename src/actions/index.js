9f9703035b390af971bca2189866bfba62cae694
Jorge Hernandez <diego.hernandez@i3television.es>
2016-12-30 15:16:04 +0100

    added action to delete a post, some markup aswell

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -14,7 +15,7 @@ export function fetchPosts(){
 	return {
 		type: FETCH_POSTS,
 		payload: request
-	}
+	};
 } 
 
 export function createPost(props){
@@ -23,7 +24,7 @@ export function createPost(props){
 	return {
 		type: CREATE_POST,
 		payload: request
-	}
+	};
 }
 
 export function fetchPost(id){
@@ -32,6 +33,14 @@ export function fetchPost(id){
 	return {
 		type: FETCH_POST,
 		payload: request
-	}
+	};
+}
 
+export function deletePost(id){
+	const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+
+	return {
+		type: DELETE_POST,
+		payload: request
+	};
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
 	componentWillMount(){
@@ -15,6 +16,7 @@ class PostsShow extends Component {
 
 		return (
 			<div>
+				<Link to="/">Back to index</Link>
 				<h3>{this.props.post.title}</h3>
 				<h6>Categories {this.props.post.categories}</h6>
 				<p>{this.props.post.content}</p>
